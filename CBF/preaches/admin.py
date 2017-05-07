f401a8caf0f23dcfbb8fd37291ef307ce580e33b
aqt01 <lowell.abbott@gmail.com>
2017-05-07 14:36:33 -0400

    Add models to admin and refinning existing preach models

--- CBF/preaches/admin.py
+++ CBF/preaches/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Preach
+from .models import Social_media
+from .models import Author
+
+admin.site.register(Preach)
+admin.site.register(Social_media)
+admin.site.register(Author)
+
+
+
+
 # Register your models here.
--- CBF/preaches/models.py
+++ CBF/preaches/models.py
@@ -1,12 +1,8 @@
 from django.db import models
-
+from datetime import datetime
 
 class Tags(models.Model):
     name = models.CharField('Categoria',max_length=80)
-
-
-class Image(models.Model):
-    img = models.ImageField('Imagen',upload_to='image')
 
 
 SOCIAL_MEDIA_NAME = (
@@ -17,26 +13,25 @@ SOCIAL_MEDIA_NAME = (
 #TODO: Define what to connect with this model
 class Social_media(models.Model):
     name = models.IntegerField('Nombre de la red social',default=0,choices=SOCIAL_MEDIA_NAME)
-    url = models.URLField()
+    url = models.URLField(max_length=200)
 
 ROLES = (
     (0, 'Pastor titular'),
-    (1, 'Pastor'),
-    (2, 'Pastora'),
-    (3, 'Diacono'),
-    (4, 'Diacona'),
-    (5, 'Miembro'),
+    (1, 'Pastor/a'),
+    (2, 'Diacono/a'),
+    (3, 'Editor/a'),
 )
 
 #TODO: Author should be part of Use model
 class Author(models.Model):
     name = models.CharField(max_length=60)
     email = models.EmailField(max_length=100)
-    social_media = models.ManyToMany(Social_media)
+    social_media = models.ManyToManyField(Social_media)
     telephone = models.CharField(max_length=12)
     cellphone = models.CharField(max_length=12)
-    img = models.ForeignKey(Image)
+    img = models.ImageField()
     role = models.IntegerField(default=5,choices=ROLES)
+
 #TODO: This should be an automatic generated model for every uploaded video on youtube
 class Preach(models.Model):
     title = models.CharField(max_length=60)
@@ -44,8 +39,7 @@ class Preach(models.Model):
     date = models.DateField()
     published_date = models.DateField(default=datetime.now())
     author = models.ForeignKey(Author)
-    tags = models.ManyToMany(Tags)
-    url = models.UrlField()
-    img = models.ImageField(Image)
+    tags = models.ManyToManyField(Tags)
+    url = models.URLField(max_length=200)
+    img = models.ImageField('Imagen',upload_to='images')
 
-# Create your models here.
