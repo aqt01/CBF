687b0354318f95207e83d989f3cc13ac904e9780
Lowell Abbott <lowell.abbott@gmail.com>
2017-05-25 14:31:15 -0700

    Add User on Author model and rename tag

--- CBF/preaches/models.py
+++ CBF/preaches/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from datetime import datetime
 
-class Tags(models.Model):
+from django.contrib.auth.models import User
+
+
+class Tag(models.Model):
     name = models.CharField('Categoria',max_length=80)
 
 
@@ -10,6 +13,7 @@ SOCIAL_MEDIA_NAME = (
     (1, 'Instagram'),
     (2, 'Twitter'),
 )
+
 #TODO: Define what to connect with this model
 class Social_media(models.Model):
     name = models.IntegerField('Nombre de la red social',default=0,choices=SOCIAL_MEDIA_NAME)
@@ -24,13 +28,13 @@ ROLES = (
 
 #TODO: Author should be part of Use model
 class Author(models.Model):
-    name = models.CharField(max_length=60)
-    email = models.EmailField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,default=0)
     social_media = models.ManyToManyField(Social_media)
     telephone = models.CharField(max_length=12)
     cellphone = models.CharField(max_length=12)
     img = models.ImageField()
     role = models.IntegerField(default=5,choices=ROLES)
+
 
 #TODO: This should be an automatic generated model for every uploaded video on youtube
 class Preach(models.Model):
@@ -39,7 +43,6 @@ class Preach(models.Model):
     date = models.DateField()
     published_date = models.DateField(default=datetime.now())
     author = models.ForeignKey(Author)
-    tags = models.ManyToManyField(Tags)
+    tags = models.ManyToManyField(Tag)
     url = models.URLField(max_length=200)
     img = models.ImageField('Imagen',upload_to='images')
-
