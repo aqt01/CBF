91c2d18d7705ca60bf1f6b39b74601a8daf4eff7
Lowell Abbott <lowell.abbott@gmail.com>
2017-05-25 18:27:42 -0700

    signature implementation for events and preaches

--- CBF/events/views.py
+++ CBF/events/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+class IndexEventView(ListView): 
+    def get_context_data(self, **kwargs):
+        return context
+
+
+class EventDetailView(DetailView):
+    #template_name = 'events_detail.html'
+    model = Event
+
+    def get_context_data(self, **kwargs):
+        context = super(EventDetailView, self).get_context_data(**kwargs)
+        context['other_events'] = Event.objects.exclude(pk=self.get_object().pk)
+        return context
+
--- CBF/preaches/views.py
+++ CBF/preaches/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+from .models import Preach
+
+class IndexPreachView(ListView):
+    model = Preach
+    #template_name = 'events_list.html'
+    paginate_by = 6
+
+
+class EventDetailView(DetailView):
+    #template_name = 'events_detail.html'
+    model = Event
+
+    def get_context_data(self, **kwargs):
+        context = super(EventDetailView, self).get_context_data(**kwargs)
+        context['other_events'] = Event.objects.exclude(pk=self.get_object().pk)
+        return context
+
+
+
+
 # Create your views here.
