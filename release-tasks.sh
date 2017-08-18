bd7de7e7873087391fea850a2ba5dce0415b3113
aqt01 <lowell.abbott@gmail.com>
2017-08-18 05:45:17 -0400

    Add correct path for loading fixtures. Still need to fix how to upload credentials files to work with youtube script

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,3 +1,3 @@
 python CBF/manage.py migrate --settings CBF.conf.production
-python manage.py loaddata CBF/fixtures.json --settings CBF.conf.production
+python CBF/manage.py loaddata CBF/fixtures.json --settings CBF.conf.production
 python CBF/manage.py populate-sermons --settings CBF.conf.production
