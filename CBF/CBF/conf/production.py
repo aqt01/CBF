589d90f66de3dd12a9b9e8b10af39c03154e5962
aqt01 <lowell.abbott@gmail.com>
2017-08-18 16:10:24 -0400

    Add correct path to custom_storages

--- CBF/CBF/conf/production.py
+++ CBF/CBF/conf/production.py
@@ -22,7 +22,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
 STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'CBF.custom_storages.StaticStorage'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 
 MEDIAFILES_LOCATION = 'media'
