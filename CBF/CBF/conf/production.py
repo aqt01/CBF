8ab3d1b899c41409241c9f3ee70c4cb9620c64a6
aqt01 <lowell.abbott@gmail.com>
2017-08-18 16:35:08 -0400

    Add DJANGO_SECRET_KEY and correct path for custom_storages

--- CBF/CBF/conf/production.py
+++ CBF/CBF/conf/production.py
@@ -13,8 +13,10 @@ INSTALLED_APPS += (
     'storages',
 )
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 # S3 AWS SETTINGS
-AWS_STORAGE_BUCKET_NAME = 'comunidadbf'
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
@@ -22,9 +24,9 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
 STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATICFILES_STORAGE = 'CBF.custom_storages.StaticStorage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 
 MEDIAFILES_LOCATION = 'media'
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+DEFAULT_FILE_STORAGE = 'CBF.custom_storages.MediaStorage'
