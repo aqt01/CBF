a4836e6bd7d90191a09471cda9b5928365d75a22
aqt01 <lowell.abbott@gmail.com>
2017-08-18 18:35:35 -0400

    Add name of the bucket directly and omitting staticfiles dirs

--- CBF/CBF/conf/base.py
+++ CBF/CBF/conf/base.py
@@ -64,9 +64,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR),
-)
+
 SITE_ID = 1
 
 
