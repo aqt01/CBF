ed3754055a02db6f613eaf56b5ee0abfcc2d61d6
aqt01 <lowell.abbott@gmail.com>
2017-08-18 15:30:34 -0400

    Add boto django-storages and mailgun to production requirements. Also add s3 bucket for media and static files storage

--- CBF/CBF/conf/production.py
+++ CBF/CBF/conf/production.py
@@ -1,4 +1,5 @@
 from .base import *
+import os
 
 DEBUG = False
 
@@ -6,4 +7,24 @@ ALLOWED_HOSTS = ['comunidadbiblicadefe.herokuapp.com']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-TEMPLATED_EMAIL_BACKEND =  'templated_email.backends.vanilla_django'
+TEMPLATED_EMAIL_BACKEND = 'templated_email.backends.vanilla_django'
+
+INSTALLED_APPS += (
+    'storages',
+)
+
+# S3 AWS SETTINGS
+AWS_STORAGE_BUCKET_NAME = 'comunidadbf'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'CBF.custom_storages.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
