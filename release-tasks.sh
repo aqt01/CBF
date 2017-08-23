74aba5f059f4cbc7d08cdb20ba5cd01182f7bef5
aqt01 <lowell.abbott@gmail.com>
2017-08-23 17:08:53 -0400

    Add correct command for loading fixtures

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,2 +1,2 @@
 python CBF/manage.py migrate --settings CBF.settings.production
-#python CBF/manage.py populate-sermons --settings CBF.settings.production
+python CBF/manage.py loaddata CBF/CBF/fixtures.json --settings CBF.settings.production
