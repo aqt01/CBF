0db69476f4be392bf86763639bac8886a1abf37a
aqt01 <lowell.abbott@gmail.com>
2017-08-23 15:44:42 -0400

    Add staging configuration with https/ssl and correct allowed hosts

--- CBF/CBF/settings/staging.py
+++ CBF/CBF/settings/staging.py
@@ -2,8 +2,24 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['comunidadbiblicadefe-staging.herokuapp.com']
+ALLOWED_HOSTS = ['comunidadbiblicadefestaging.herokuapp.com', 'staging.comunidadbiblicadefe.org']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 TEMPLATED_EMAIL_BACKEND =  'templated_email.backends.vanilla_django'
+
+# HTTPS CONFIG
+
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# HSTS CONFIG
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_SECONDS = 31536000
+
+# COOKIE SECURE CONFIG
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
