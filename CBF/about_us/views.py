d6db268adbe7c0150e038aea6b48dcf90621e97b
aqt01 <lowell.abbott@gmail.com>
2017-08-24 01:12:14 -0400

    Abstracted the context returning in about_us view to a mixing and add try catch in case the banners are not added

--- CBF/about_us/views.py
+++ CBF/about_us/views.py
@@ -4,14 +4,21 @@ from membership.models import Member
 from widgets.models import PrincipleGroup, BannerGroup
 
 
-class IndexAboutUsView(TemplateView):
-    template_name = 'CBF/about-us.html'
+class IndexAboutUsMixinView(object):
 
     def get_context_data(self, **kwargs):
-        context = super(IndexAboutUsView, self).get_context_data(**kwargs)
+        context = super(IndexAboutUsMixinView, self).get_context_data(**kwargs)
         context['main_leaders'] = Member.objects.filter(role__lte=2)
         context['leaders'] = Member.objects.filter(role__gt=2, role__lte=4)
-        context['principle_group'] = PrincipleGroup.objects.filter(extended_object__application_namespace='about_us').first()
-        banner_group = BannerGroup.objects.filter(extended_object__application_namespace='about_us').first()
-        context['banner'] = banner_group.banner_set.first()
+        try:
+            context['principle_group'] = PrincipleGroup.objects.filter(extended_object__application_namespace='about_us').first()
+            banner_group = BannerGroup.objects.filter(extended_object__application_namespace='about_us').first()
+            context['banner'] = banner_group.banner_set.first()
+        except Exception:
+            context['principle_group'] = None
+            context['banner'] = None
         return context
+
+
+class IndexAboutUsView(IndexAboutUsMixinView, TemplateView):
+    template_name = 'CBF/about-us.html'
