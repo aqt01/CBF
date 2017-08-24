b2e083bddc4325e5bae996837aa8351c7f2144b7
aqt01 <lowell.abbott@gmail.com>
2017-08-24 04:57:22 -0400

    Created template tags for youtube urls to get it for the embed modals. Add a modal for viewing sermons. Clarifying tag names on base.html (js instead of jquery) and add additional_js in case of more javascript code

--- CBF/CBF/templatetags/CBF_filters.py
+++ CBF/CBF/templatetags/CBF_filters.py
@@ -5,5 +5,12 @@ def get_element_name(element):
     return str(element._meta.verbose_name_plural)
 
 
+def get_embed_url(url):
+    youtube_video_code = url.split('=').pop()
+    embed_url = "https://www.youtube.com/embed/" + youtube_video_code
+    return embed_url
+
+
 register = template.Library()
 register.filter('get_element_name', get_element_name)
+register.filter('get_embed_url', get_embed_url)
