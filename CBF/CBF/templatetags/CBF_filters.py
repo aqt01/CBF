831597e3f6145e59ccf3545682b8703231a5d3f4
aqt01 <lowell.abbott@gmail.com>
2017-08-29 04:10:17 -0400

    Add to thoughts urls the name of the app. Also add all elements to work with % url %. And add facebook share with correct route for cards and last-message

--- CBF/CBF/templatetags/CBF_filters.py
+++ CBF/CBF/templatetags/CBF_filters.py
@@ -5,6 +5,10 @@ def get_element_name(element):
     return str(element._meta.verbose_name_plural)
 
 
+def get_url_detail_app(element):
+    return str(element._meta.app_label + ':detail')
+
+
 def get_embed_url(url):
     youtube_video_code = url.split('=').pop()
     embed_url = "https://www.youtube.com/embed/" + youtube_video_code
@@ -13,4 +17,5 @@ def get_embed_url(url):
 
 register = template.Library()
 register.filter('get_element_name', get_element_name)
+register.filter('get_url_detail_app', get_url_detail_app)
 register.filter('get_embed_url', get_embed_url)
--- CBF/thoughts/urls.py
+++ CBF/thoughts/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+
+app_name = 'thoughts'
+
 urlpatterns = [
-    url(r'^$', views.IndexThoughtView.as_view(), name="thought_index"),
-    url(r'^(?P<slug>[-\w]+)/$', views.ThoughtDetailView.as_view(), name="thought_detail"),
+    url(r'^$', views.IndexThoughtView.as_view(), name="index"),
+    url(r'^(?P<slug>[-\w]+)/$', views.ThoughtDetailView.as_view(), name="detail"),
 ]
