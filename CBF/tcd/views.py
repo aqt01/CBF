7897dfaf5d73be5e8604abe62fded2188cfcfc90
Lowell Abbott <lowell@instacarro.com>
2018-03-18 11:25:23 -0400

    Add visual improvements to TCD

--- CBF/tcd/views.py
+++ CBF/tcd/views.py
@@ -11,17 +11,13 @@ class TCDSubscriptionView(TemplateView):
     template_name = 'TCD/index.html'
 
 
-class TCDRegisterView(FormView):
+class TCDRegisterView(TemplateView, FormView):
     form_class = TCDSubscriptionForm
     model = TCDSubscription
     slug_field = 'uuid'
     slug_url_kwarg = 'uuid'
     template_name = 'TCD/form.html'
     success_url = reverse_lazy('tcd:thanks')
-
-    def get_context_data(self, **kwargs):
-        context = super(TCDRegisterView, self).get_context_data(**kwargs)
-        return render(self.request,self.template_name,context)
 
 
 class TCDSubscriptionViewThanks(TemplateView):
